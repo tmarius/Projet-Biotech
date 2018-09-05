256f63111831b7cf16d451de7268c4ccfdfa8b34
tmarius <thomas.marius@hotmail.fr>
2018-09-05 17:05:42 +0200

    ABRICOTTTTTTT

--- Programme.py
+++ Programme.py
@@ -8,13 +8,22 @@ Created on Wed Sep  5 14:34:26 2018
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#driver = webdriver.Chrome()
+#driver.get("http://www.python.org")
+#assert "Python" in driver.title
+#elem = driver.find_element_by_name("q")
+#elem.clear()
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+#driver.close()
+
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
+driver.get("https://www.wikipedia.org/")
+elem = driver.find_element_by_id("searchInput")
+elem.send_keys("abricot")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
-driver.close()
-
+recup = driver.find_element_by_class_name("magnify")
+print (recup)
+#driver.close()
