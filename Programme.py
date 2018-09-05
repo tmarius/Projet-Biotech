2cc51b62629b427a870fee9cd235c60a7a756719
tmarius <thomas.marius@hotmail.fr>
2018-09-05 15:39:24 +0200

    premier commit

---
+++ Programme.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 14:34:26 2018
+
+@author: Thomas
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
+
