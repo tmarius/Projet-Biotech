fd05af83351862802a8103fb80c50bba0fdb3de7
tmarius <33683843+tmarius@users.noreply.github.com>
2018-09-07 09:54:54 +0200

    Le programme V1 _ ABRICOT

--- Programme.py
+++ Programme.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep  5 14:34:26 2018
+Created on Fri Sep  7 08:47:25 2018
 
+@author: Thomas
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 14:34:26 2018
 @author: Thomas
 """
 
@@ -12,12 +18,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException 
+from os import chdir,getcwd 
 import time
 import re
 
-driver = webdriver.Chrome()
 
-
+#####################################################FIND GENE ID#########################"
 def searchByNameId(espece, nameId):
     driver.get("https://www.ncbi.nlm.nih.gov/gene/")
     elem = driver.find_element_by_id("term")
@@ -38,7 +44,7 @@ def searchByNameId(espece, nameId):
     motif = re.compile(r'(?<=Gene ID: )[0-9]+')
     gene_id = motif.search(chaine_id)
     gene_id = gene_id.group(0)
-    print (gene_id)
+    return (gene_id)
 
 def searchBySeq(espece, seq):
     driver.get("https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=blastn&PAGE_TYPE=BlastSearch&LINK_LOC=blasthome")
@@ -69,9 +75,87 @@ def searchBySeq(espece, seq):
     motif = re.compile(r'(?<=GenBank: )([A-Za-z]|[0-9])+')
     gene_id = motif.search(chaine_id)
     gene_id = gene_id.group(0)
-    print (gene_id)
+    return (gene_id)
 
-searchByNameId("Vitis vinifera","Vvht5")
-searchBySeq('Vitis vinifera (taxid:29760)','AATCAAGCATTTTATCAAATCAATTTGTTTAAACAGATGATGTGATGAAAGCCGTTTCTTTCTTACCTGTGTATTGGGAATTTAATACCGTAATATTTTGTATCATATACACACGTGTATATATATATATATATATATATATATATATGAAAATGTCTTTATCACATTA')
 
-   
+#########################################################FIND CDS##########################################
+    
+
+def findCDS(gene_id):
+    driver.get("https://www.ncbi.nlm.nih.gov/gene/")
+    query = driver.find_element_by_id("term")
+    query.send_keys(gene_id)
+    query.send_keys(Keys.RETURN)
+    time.sleep(1)
+    #Recupere le premier mRNA car souvent le bon, a verifié => Sinon faire comme au dessus et demander selection
+    mrna = driver.find_element_by_xpath("//*[@id='proteinTblId']/tbody//td[contains(text(), 'mRNA')]/following-sibling::td").text
+    mrna = driver.find_element_by_xpath("//*[@id='proteinTblId']/tbody//td[contains(text(), 'mRNA')]/following-sibling::td/a").click()
+    #PARTIE 1
+    #Permet de récupérer les positions de début et fin du CDS
+    #On a par exemple CDS 55..1870 , on veut que begin=55 et end=1870
+    #driver.get("https://www.ncbi.nlm.nih.gov/nuccore/66016960")
+    time.sleep(2)
+    positions=driver.find_element_by_xpath("//span[contains(text(),'..')]/following-sibling::span/following-sibling::span").text
+    motif = re.compile(r'((\d+)\.\.(\d+))', re.IGNORECASE)
+    res = motif.findall(positions)
+    debut=res[0][1]
+    fin=res[0][2]    
+    time.sleep(1)
+    dropdown = driver.find_element_by_id("EntrezSystem2.PEntrez.Nuccore.Sequence_ResultsPanel.Sequence_SingleItemSupl.Sequence_ViewerGenbankSidePanel.Sequence_ViewerChangeRegion.Shutter").click()
+    time.sleep(1)
+    selected = driver.find_element_by_id("crselregion").click()
+    debinput = driver.find_element_by_id("crfrom")
+    debinput.send_keys(debut)
+    fininput = driver.find_element_by_id("crto")
+    fininput.send_keys(fin)
+    upd = driver.find_element_by_id("updateselregion").click()
+    time.sleep(2)
+    tofasta = driver.find_element_by_id("ReportShortCut6").click()
+    time.sleep(2)
+    #sequence = driver.find_element_by_xpath("//*[@id='viewercontent1']")
+    #PARTIE 2
+    #Permet de récuperer le CDS en fasta
+#    driver.get("https://www.ncbi.nlm.nih.gov/nuccore/AY538261.1?report=fasta&from=55&to=1665")
+    seq=driver.find_element_by_xpath("//pre").text
+    return (seq)
+
+#######################################################CRISPR SGRNA###########################################
+def findSGRNA(seq):
+    path = getcwd(); 
+    print (path);
+    driver.get("https://crispr.dbcls.jp/")
+    
+#    seq="ATGATTCATTCAATTGTTGGTGCCATTACTGGCGAAAATGATAAGAAGAAGATCAAGGGAACTGTTGTGTTGATGAAGAAGAATGTGTTGGATTTTAATGACTTCAATGCATCGGTTCTGGACCGGGTTCATGAGCTGTTGGGACAGGGAGTCTCTCTGCAGCTCGTCAGTGCTGTTCATGGTGATCCTGCAAATGGGTTACAGGGGAAACTTGGGAAACCAGCATACTTGGAAGACTGGATTACCACAATTACTTCTTTAACCGCTGGCGAGTCTGCATTCAAGGTCACGTTCGACTGGGATGAGGAGATTGGAGAGCCAGGGGCTTTCATAATTAGAAACAATCACCACAGTGAGTTTTACCTCAGGACTCTCACTCTTGAAGATGTTCCTGGGTGTGGCAGAATTCA CTTTGTTTGTAATTCCTGGGTCTACCCTGCTAAGCACTACAAAACTGACCGTGTTTTCTTCACTAATCAGACATATCTTCCAAGTGAAACACCAGGGCCACTGCGCAAGTACAGAAAAGGGGAACTGGTGAATCTGAGGGGAGATGGAACCGGAGAGCTTAAGGAATGGGATCGAGTGTATGACTATGCTTACTATAATGATTTGGGGAATCCAGACAGGGATCTCAAATACGCCCGCCCTGTGCTGGGAGGATCTGCAGAGTATCCTTATCCCAGGAGGGGAAGAACTGGTAGACCACCATCTGAAAAAGATCCCAACACCGAGAGCAGATTGCCACTTGTGATGAGCTTAAACATATATGTTCCAAGAGATGAACGATTTGGTCACCTCAAGATGTCAGACTTCCTGGCTTATGCCCTGAAATCCATAGTTCAATTCCTTCTCCCTGAGTTTGAGGCTCTATGTGACATCACCCCCAATGAGTTTGACAGCTTCCAAGATGTATTAGACCTCTACGAAGGAGGAATCAAGGTCCCAGAGGGCCCTTTACTTGACAAAATTAAGGACAACATCCCTCTTGAGATGCTCAAGGAACTTGTTCGTACTGATGGGGAACATCTCTTCAAGTTCCCAATGCCCCAAGTCATCAAAGAGGATAAGTCTGCATGGAGGACTGACGAAGAATTTGCTAGAGAAATGCTGGCTGGACTCAACCCAGTTGTCATCCGACTACTCCAAGAGTTTCCTCCAAAAAGCAAGCTGGATCCTGA"
+#    
+    elemchampseq = driver.find_element_by_id("useq")
+    elemchampseq.clear()
+    elemchampseq.send_keys(seq)
+    elemspecificitycheck = driver.find_element_by_id("crisprdirect-combobox-dblist-inputEl")
+    elemspecificitycheck.clear()
+    elemspecificitycheck.send_keys("Grape (Vitis vinifera) genome, IGGP_12x (Jun, 2011)")
+    elemspecificitycheck.send_keys(Keys.RETURN)
+    
+    #driver.find_element_by_xpath("//div[contains(@id,'boundlist-1010-listEl')]/ul/li").click()
+    
+    driver.find_element_by_xpath("//*[@id='ext-gen1018']/form/div[1]/p[5]/input").click()
+    #driver.find_element_by_xpath("//input[contains(@type,'submit')]")
+    time.sleep(1)
+    #driver.find_element_by_xpath("//input[contains(@id,'filter_highlight')]").click()
+    #driver.find_element_by_xpath("//*[@id='ext-gen1018']/a[2][contains(text(),'Download')]").click()
+    driver.find_element_by_xpath("//*[@id='ext-gen1018']/form/div[4]/ul/li[2]/a[2]").click()
+    #driver.find_element_by_xpath("//a[contains(@type,'submit')]")
+    time.sleep(9)
+    config = json.loads(open('C:/Users/Thomas/Downloads/CRISPRdirect.json').read())
+    results=config['results']
+
+#######################################################DEBUT PROGRAMME##############################################
+driver = webdriver.Chrome()  
+
+gene_id = searchByNameId("Vitis vinifera","Vvht5")
+print (gene_id)
+##gene_id2 = searchBySeq('Vitis vinifera (taxid:29760)','AATCAAGCATTTTATCAAATCAATTTGTTTAAACAGATGATGTGATGAAAGCCGTTTCTTTCTTACCTGTGTATTGGGAATTTAATACCGTAATATTTTGTATCATATACACACGTGTATATATATATATATATATATATATATATATGAAAATGTCTTTATCACATTA')
+##print (gene_id2)
+seq = findCDS(gene_id)
+print (seq)
+
+findSGRNA(seq)
